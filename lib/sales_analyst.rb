37efb57cf24b59f26f1fac2e36f6f0d533b5a430
Luke Chambers <lukenatchambers@gmail.com>
2018-01-05 09:17:45 -0700

    Refactor stdev

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -19,19 +19,13 @@ class SalesAnalyst
   end
 
   def average_items_per_merchant_standard_deviation
-    average_items_per_merchant
-    first_calc = @sales_engine.merchants.all.map do |merchant|
-    (@sales_engine.items.find_all_by_merchant_id(merchant.id).count - average_items_per_merchant) ** 2
+    mean = merchants.all.map do |merchant|
+     mean_calculation(merchant)
     end
-    calculate_stdev(first_calc)
-  end
-
-  def calculate_stdev(first_calc)
-    total_div = first_calc.count - 1
-    variance = first_calc.sum / total_div
+    total_div = mean.count - 1
+    variance = mean.sum / total_div
     stdev = Math.sqrt(variance).round(2)
   end
-
 
   def average_item_price_for_merchant(merchant_id)
     item_per_merchant = @sales_engine.find_item_by_merchant_id(merchant_id)
@@ -42,10 +36,21 @@ class SalesAnalyst
   end
 
   def average_average_price_per_merchant
-    total = @sales_engine.merchants.all.sum do |merchant|
+    total = merchants.all.sum do |merchant|
       average_item_price_for_merchant(merchant.id)
     end
-    (total / @sales_engine.merchants.all.count).round(2)
+    (total / merchants.all.count).round(2)
   end
 
+  def merchants
+    @sales_engine.merchants
+  end
+
+  def items
+    @sales_engine.items
+  end
+
+  def mean_calculation(merchant)
+    (items.find_all_by_merchant_id(merchant.id).count - average_items_per_merchant) ** 2
+  end
 end
