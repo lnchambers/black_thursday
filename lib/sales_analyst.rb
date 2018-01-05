2dc612e175a9ea7dd04dd1681db4fe29861ff817
Luke Chambers <lukenatchambers@gmail.com>
2018-01-04 17:49:05 -0700

    Work on SalesAnalyst average

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -7,8 +7,15 @@ class SalesAnalyst
   end
 
   def average_items_per_merchant
+    (total_items / total_merchants).round(2)
+  end
 
+  def total_merchants
+    @sales_engine.merchants.all.count.to_f
+  end
 
+  def total_items
+    @sales_engine.items.all.count.to_f
   end
 
 
