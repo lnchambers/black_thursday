1228fbf842b73f52da29a7468675390289c4968d
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-01-04 17:43:43 -0700

    good start on sales_analyst

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -1,6 +1,43 @@
 require_relative 'test_helper'
 require './lib/sales_analyst'
+require './lib/sales_engine'
 
 class SalesAnalystTest < Minitest::Test
+
+  def test_it_exists
+    sales_engine = mock('this')
+    sa = SalesAnalyst.new(sales_engine)
+    assert_instance_of SalesAnalyst, sa
+  end
+
+  def test_find_total_merchants
+    sales_engine = SalesEngine.from_csv({
+      :items     => "./test/fixtures/item_fixture.csv",
+      :merchants => "./test/fixtures/merchant_fixture.csv",
+    })
+    sa = SalesAnalyst.new(sales_engine)
+
+    assert_equal 40, sa.total_merchants
+  end
+
+  def test_find_total_items
+    sales_engine = SalesEngine.from_csv({
+      :items     => "./test/fixtures/item_fixture.csv",
+      :merchants => "./test/fixtures/merchant_fixture.csv",
+    })
+    sa = SalesAnalyst.new(sales_engine)
+
+    assert_equal 40, sa.total_items
+  end
+
+  def test_find_average_items_per_merchant
+    sales_engine = SalesEngine.from_csv({
+      :items     => "./test/fixtures/item_fixture.csv",
+      :merchants => "./test/fixtures/merchant_fixture.csv",
+    })
+    sa = SalesAnalyst.new(sales_engine)
+
+    assert_equal 1, sa.average_items_per_merchant
+  end
 
 end
