03120e5e50a954183c219e62d483881aeccb75b0
Luke Chambers <lukenatchambers@gmail.com>
2018-01-04 19:02:40 -0700

    Create averages for sales_analyst

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -18,5 +18,23 @@ class SalesAnalyst
     @sales_engine.items.all.count.to_f
   end
 
+  def i_so_pale
+    system('say ~ I so pale')
+  end
+
+  def average_item_price_for_merchant(merchant_id)
+    item_per_merchant = @sales_engine.find_item_by_merchant_id(merchant_id)
+    total = item_per_merchant.sum do |price|
+      price.unit_price
+    end
+    (total / item_per_merchant.count).round(2)
+  end
+
+  def average_average_price_per_merchant
+    total = @sales_engine.merchants.all.sum do |merchant|
+      average_item_price_for_merchant(merchant.id)
+    end
+    (total / @sales_engine.merchants.all.count).round(2)
+  end
 
 end
