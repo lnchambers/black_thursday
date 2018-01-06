7e392b63e249d42c7649a6499949df3371fd587e
Luke Chambers <lukenatchambers@gmail.com>
2018-01-06 10:33:07 -0700

    Work on it2 framework

--- test/invoice_repo_test.rb
+++ test/invoice_repo_test.rb
@@ -3,21 +3,5 @@ require './lib/invoice_repo'
 
 class InvoiceRepoTest < Minitest::Test
 
-  # def setup
-  #   @sales_engine ||= SalesEngine.from_csv({
-  #     items: 'test/fixtures/item_fixture.csv',
-  #     merchants: 'test/fixtures/merchant_fixture.csv',
-  #     invoices: 'test/fixtures/invoice_fixture.csv'
-  #     })
-  #   @invoices = @sales_engine.invoices
-  # end
-
-  def test_it_exists
-    assert_instance_of InvoiceRepo, @invoices
-  end
-
-  def test_invoice_repo_all
-    assert_equal "", @invoices.all
-  end
 
 end
