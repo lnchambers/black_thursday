dd8e7c78fec3bcf476196a3a1303a6a7094ee0ca
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-01-06 14:55:53 -0700

    going to take break, work on tests

--- lib/item_analyst_mod.rb
+++ lib/item_analyst_mod.rb
@@ -18,10 +18,6 @@ module ItemAnalyst
     end
   end
 
-  def item_price_mean
-    all_items_price / total_items
-  end
-
   def item_variance
     all_items.map do |item|
     (item[1].unit_price - item_price_mean) ** 2
