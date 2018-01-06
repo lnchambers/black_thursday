fc62388264606117b7dd874fbedf61f2f722468d
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-01-06 16:07:40 -0700

    item repo test shows find_all_by_price to work

--- test/item_repo_test.rb
+++ test/item_repo_test.rb
@@ -18,13 +18,15 @@ class ItemRepoTest < Minitest::Test
     assert_equal 40, @items.all.count
   end
 
-  def test_find_merchant_by_merchant_id
+  def test_find_merchants_by_merchant_id
     desired_merchant1 = @sales_engine.merchants.merchants[1]
+    desired_merchant2 = @sales_engine.merchants.merchants[2]
 
     assert_equal desired_merchant1, @items.find_merchant(1)
+    assert_equal desired_merchant2, @items.find_merchant(2)
   end
 
-  def test_find_by_item_id
+  def test_find_items_by_item_id
     desired_item1 = @items.items[1]
     desired_item2 = @items.items[2]
 
@@ -47,11 +49,12 @@ class ItemRepoTest < Minitest::Test
   end
 
   def test_find_all_by_price
-    skip
-    desired_item1 = @items.items[2]
-    desired_item2 = @items.items[17]
+    desired_item1 = @items.items[1]
+    desired_item2 = @items.items[3]
+    desired_item3 = @items.items[37]
+    desired_items = [desired_item1, desired_item2, desired_item3]
 
-    assert_equal [desired_item1, desired_item2], @items.find_all_by_price(BigDecimal.new(900))
+    assert_equal desired_items, @items.find_all_by_price((0.7000).round(4))
   end
 
   def test_find_all_by_price_in_range
