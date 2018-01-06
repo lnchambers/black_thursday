e9dbcab81c9a52c22ada90bf287c6af9336eeaa1
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-01-06 14:26:19 -0700

    still has errors don't merge! just check it out

---
+++ lib/merchant_analyst_mod.rb
@@ -0,0 +1,15 @@
+module MerchantAnalyst
+
+  def merchants
+    @sales_engine.merchants
+  end
+
+  def all_merchants
+    merchants.merchants
+  end
+
+  def total_merchants
+    @sales_engine.merchants.all.count.to_f
+  end
+
+end
