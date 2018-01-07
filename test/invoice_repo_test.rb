be03bb8952aa5fc97c7efdc974bb1d1569314fba
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-01-06 18:40:51 -0700

    find_all_by_customer_id now tested on invoice_repo test

--- test/invoice_repo_test.rb
+++ test/invoice_repo_test.rb
@@ -17,11 +17,23 @@ class InvoiceRepoTest < Minitest::Test
     assert_equal 40, @invoices.all.count
   end
 
-  def test_that_find_by_id_returns_invoice_by_id
+  def test_that_find_by_id_method_returns_invoice_by_id
     desired_invoice = @invoices.invoices[1]
 
     assert_instance_of Invoice, @invoices.find_by_id(1)
     assert_equal desired_invoice, @invoices.find_by_id(1)
+  end
+
+  def test_find_all_invoice_by_customer_id_method_returns_correct_invoice
+    desired_invoice1 = @invoices.invoices[13]
+    desired_invoice2 = @invoices.invoices[14]
+    desired_invoice3 = @invoices.invoices[15]
+    desired_invoices = [desired_invoice1, desired_invoice2, desired_invoice3]
+
+    assert_equal desired_invoices, @invoices.find_all_by_customer_id(3)
+    desired_invoices.each do |invoice|
+      assert_instance_of Invoice, invoice
+    end  
   end
 
 
