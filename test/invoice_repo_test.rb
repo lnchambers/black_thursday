89ed166fcf880506ca7f902f2ede3d9103ca0182
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-01-06 18:28:56 -0700

    invoice_all method tested

--- test/invoice_repo_test.rb
+++ test/invoice_repo_test.rb
@@ -3,5 +3,19 @@ require './lib/invoice_repo'
 
 class InvoiceRepoTest < Minitest::Test
 
+  def setup
+    @sales_engine ||= SalesEngine.from_csv({
+      items: 'test/fixtures/item_fixture.csv',
+      merchants: 'test/fixtures/merchant_fixture.csv',
+      invoices: './test/fixtures/invoice_fixture.csv'
+      })
+    @invoices ||= @sales_engine.invoices
+  end
+
+  def test_that_all_method_returns_all_invoices
+    assert_instance_of Invoice, @invoices.all.first
+    assert_equal 40, @invoices.all.count
+  end
+
 
 end
