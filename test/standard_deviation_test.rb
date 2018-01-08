2485b1fcc07375cce88269022053b75249d5e519
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-01-08 16:23:36 -0700

    blah

--- test/standard_deviation_test.rb
+++ test/standard_deviation_test.rb
@@ -3,7 +3,7 @@ require './lib/standard_deviation'
 
 class StandardDeviationTest < Minitest::Test
   include StandardDeviation
-
+  
   def test_standard_deviation_mean
     assert_equal 15, mean(150, 10)
   end
