5c4ac8484192505b77543eacf9b3090487b3a5a5
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-01-07 20:58:13 -0700

    all pre-built customers methods tested

--- test/customer_repo_test.rb
+++ test/customer_repo_test.rb
@@ -1,8 +1,9 @@
-require_relative 'customer_repo'
-require_relative 'sales_engine'
+require './lib/customer_repo'
+require './lib/sales_engine'
 require_relative 'test_helper'
 
 class CustomerRepositoryTest < Minitest::Test
+  attr_reader :customers
 
   def setup
     @sales_engine ||= SalesEngine.from_csv({
@@ -16,5 +17,19 @@ class CustomerRepositoryTest < Minitest::Test
     @customers ||= @sales_engine.customers
   end
 
+  def test_all_returns_array_of_customer_instances
+    assert_instance_of Array, customers.all
+    assert_instance_of Customer, customers.all.first
+    assert_equal 40, customers.all.count
+  end
+
+  def test_find_by_id_returns_correct_customer
+    assert_nil customers.find_by_id(99999)
+
+    desired_customer = customers.customers[2]
+
+    assert_instance_of Customer, customers.find_by_id(2)
+    assert_equal desired_customer, customers.find_by_id(2)
+  end
 
 end
