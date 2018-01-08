ddab6706a173a9d5d87f77e412abf2fc4fcecad4
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-01-08 14:36:51 -0700

    fixed some stupid stuff in invoice_item_repo_test

--- test/transaction_repo_test.rb
+++ test/transaction_repo_test.rb
@@ -4,23 +4,25 @@ require './lib/sales_engine'
 require './lib/item_repo'
 
 class TransactionRepoTest < MiniTest::Test
-  attr_reader :transaction
+  attr_reader :transactions
+
 
   def setup
     @sales_engine ||= SalesEngine.from_csv({
       items: 'test/fixtures/item_fixture.csv',
-      merchant: 'test/fixtures/merchant_fixture.csv',
+      merchants: 'test/fixtures/merchant_fixture.csv',
       invoices: './test/fixtures/invoice_fixture.csv',
       invoice_items: './test/fixtures/invoice_item_fixture.csv',
       customers: './test/fixtures/customer_fixture.csv',
       transactions: './test/fixtures/transaction_fixture.csv'
       })
-    @transaction = @sales_engine.transaction
+    @transactions ||= @sales_engine.transactions
   end
 
   def test_transaction_repo_attributes
-    assert_instance_of Hash, transaction
-    assert_instance_of Transaction, transaction.values.first
+    assert_instance_of TransactionRepo, transactions
+    assert_instance_of Hash, transactions.transactions
+    assert_instance_of Transaction, transactions.transactions.values.first
     assert_instance_of SalesEngine, transactions.parent
   end
 end
